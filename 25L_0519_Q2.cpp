d81b90412e3405fefbf970726444a68c07efcb1f
aliiasad <alii.asad0519@gmail.com>
2026-03-10 01:49:00 +0500

    Assignment Operator Overloaded Partially

---
+++ 25L_0519_Q2.cpp
@@ -0,0 +1,62 @@
+# include <iostream>
+using namespace std;
+
+class Matrix {
+    private:
+        int size;
+        int** matrix;
+    public:
+        Matrix (); // default constructor
+        Matrix (int); // parameterized constructor
+        Matrix (const Matrix&); // copy constructor 
+        Matrix& operator= (const Matrix&); // overloaded = operator
+        ~Matrix (); // destructor
+
+
+};
+
+Matrix :: Matrix () {
+    size = 2; // given is 2x2
+    matrix = new int* [size];
+    for (int i = 0; i < size; i++)  {
+        *(matrix + i) = new int [size];
+        for (int j = 0; j < size; j++)  {
+            *(*(matrix + i) + j) = 0;
+        }  
+    }
+}
+
+Matrix :: Matrix (int n) {
+    size = n;
+    matrix = new int* [size];
+    for (int i = 0; i < size; i++)  {
+        *(matrix + i) = new int [size];
+        for (int j = 0; j < size; j++)  {
+            *(*(matrix + i) + j) = 0;
+        }
+    }
+}
+
+Matrix :: Matrix (const Matrix& dummy)  {
+    size = dummy.size;
+    matrix = new int* [size];
+    for (int i = 0; i < size; i++)  {
+        *(matrix + i) = new int [size];
+        for (int j = 0; j < size; j++)  {
+            *(*(matrix + i) + j) = *(*(dummy.matrix + i) + j);
+        }
+    }
+}
+
+Matrix& Matrix :: operator= (const Matrix& dummy)  {
+    if (this == &dummy) return *this;
+
+    for (int i = 0; i < size; i++)  {
+        delete[] *(matrix + i);
+    }
+    delete[] matrix;
+}
+int main()  {
+
+    return 0;
+}
